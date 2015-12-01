a1fbfaf99345463bc045b0aa481da5b434ce3fa6
Pau Farre <pau.farre@ac.upc.edu>
2015-12-01 08:43:44 +0100

    Implemented some tests with gtest

--- test/cacti-tests.cpp
+++ test/cacti-tests.cpp
@@ -4,14 +4,75 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <memory>
+
 #include <io.hpp>
 
-TEST(cacti, main) {
+// RAII
+class cout_redirect {
+public:
+    cout_redirect(std::streambuf* new_buffer)
+            : old(std::cout.rdbuf(new_buffer)) {
+    }
+
+    ~cout_redirect() {
+        std::cout.rdbuf(old);
+    }
+
+private:
+    std::streambuf* old;
+};
+
+TEST(cacti, multiple_runs_same_output_cache) {
     string infile_name("config/cache.cfg");
 
-    cacti_interface(infile_name);
+    std::string a;
+    {
+        std::stringstream buffer;
+        cout_redirect redirect(buffer.rdbuf());
 
-    //TODO compare result with a 'reference-result'
+        cacti_interface(infile_name);
+
+        a = buffer.str();
+    }
+
+    std::string b;
+    {
+        std::stringstream buffer;
+        cout_redirect redirect(buffer.rdbuf());
+
+        cacti_interface(infile_name);
+
+        b = buffer.str();
+    }
+
+    EXPECT_TRUE(a == b) << "First run:\n" << a << "Second run:\n" << b;
+}
+
+TEST(cacti, multiple_runs_same_output_dram) {
+    string infile_name("config/dram.cfg");
+
+    std::string a;
+    {
+        std::stringstream buffer;
+        cout_redirect redirect(buffer.rdbuf());
+
+        cacti_interface(infile_name);
+
+        a = buffer.str();
+    }
+
+    std::string b;
+    {
+        std::stringstream buffer;
+        cout_redirect redirect(buffer.rdbuf());
+
+        cacti_interface(infile_name);
+
+        b = buffer.str();
+    }
+
+    EXPECT_TRUE(a == b) << "First run:\n" << a << "Second run:\n" << b;
 }
 
 int main(int argc, char* argv[]) {
